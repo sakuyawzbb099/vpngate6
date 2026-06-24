a22ae17491d3fe9ad63d5256fe2b68205c20f782
root <root@en-cvm-3p47qe523i223>
2026-06-24 23:57:03 +0800

    fix missing comma after inactive

--- vpngate6_multi.py
+++ vpngate6_multi.py
@@ -147,7 +147,7 @@ def setup_policy_routing(tun_dev: str, table: int):
 def openvpn_cmd(config_file: str, tun_dev: str) -> list[str]:
     cmd = ["openvpn","--config",config_file,"--dev",tun_dev,"--dev-type","tun",
            "--pull-filter","ignore","route-ipv6","--pull-filter","ignore","ifconfig-ipv6",
-           "--pull-filter","ignore","inactive"
+           "--pull-filter","ignore","inactive",
            "--route-delay","2","--connect-retry-max","999","--connect-timeout","15",
            "--keepalive","10","60",
            "--resolv-retry","infinite",
